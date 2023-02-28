b59bca8aa2c9422b72faa8469f3cb3608c76efe6
Juan2579 <juandavidgualdronnavarro@gmail.com>
2023-02-28 13:51:42 -0500

    Fixed logo url

--- src/containers/Nav.jsx
+++ src/containers/Nav.jsx
@@ -11,7 +11,7 @@ export const Nav = ({setShowMobileMenu}) => {
   }, [showMenu])
   return (
     <nav ref={navRef} className="w-full h-full relative flex justify-between items-center z-50 md:px-20 py-3 gap-5">
-      <img className="first-load transition-all duration-300 w-16 mx-5 md:mx-0" src="/assets/images/logo.png" alt="Juan David Logo" />
+      <img className="first-load transition-all duration-300 w-16 mx-5 md:mx-0" src="assets/images/logo.png" alt="Juan David Logo" />
       <NavbarDesktop />
       <NavMobile setShowMenu={setShowMenu} showMenu={showMenu} navRef={navRef} />
       <MenuIcons showMenu={showMenu} setShowMenu={setShowMenu} />
